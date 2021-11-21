a47df28c6b3b4beaab0f07228c98417de93bc590
Ben10Harper <bennyharper@gmail.com>
2021-11-20 17:54:22 -0700

    added basic layout for activity

---
+++ app/src/main/java/com/example/simpletotp/ViewEntryActivity.kt
@@ -0,0 +1,21 @@
+package com.example.simpletotp
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewentry)
+
+    }
+
+}
